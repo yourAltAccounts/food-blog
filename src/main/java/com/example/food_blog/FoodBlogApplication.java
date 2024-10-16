468bc599df33085032214f10752a56733df30481
XS <aq037997@gmail.com>
2024-10-15 21:13:12 -0700

    first commit

---
+++ src/main/java/com/example/food_blog/FoodBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBlogApplication.class, args);
+	}
+
+}
